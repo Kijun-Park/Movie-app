2b7e5b6f225dffcdff4ae86c54e40f146d18f96f
Kijun-Park <ekdlwns3685@naver.com>
2019-02-01 18:27:14 +0900

    added some css

--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import LinesEllipsis from "react-lines-ellipsis";
 import "./Movie.css";
 
 function Movie({ title, poster, genres, synopsis }) {
   return (
     <div className="Movie">
-      <div className="Movie__Columns">
+      <div className="Movie__Column">
         <MoviePoster poster={poster} alt={title} />
       </div>
-      <div className="Movie__Columns">
+      <div className="Movie__Column">
         <h1>{title}</h1>
-        <div className="Movie__Genres" />
-        {genres.map((genre, index) => (
-          <MovieGenre genre={genre} key={index} />
-        ))}
+        <div className="Movie__Genres">
+          {genres.map((genre, index) => (
+            <MovieGenre genre={genre} key={index} />
+          ))}
+        </div>
+        <div className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine="5"
+            ellipsis=" ..."
+            trimRight
+            basedOn="letters"
+          />
+        </div>
       </div>
-      <p className="Movie_Synopsis">{synopsis}</p>
     </div>
   );
 }
